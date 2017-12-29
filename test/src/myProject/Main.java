d03889e771d5422ebe61708f1f80da083c17f3ff
ardianxhafa <ardian_xhafa@hotmail.com>
2017-12-29 01:25:02 +0100

    Changes in main

--- test/src/myProject/Main.java
+++ test/src/myProject/Main.java
@@ -3,7 +3,11 @@ package myProject;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Animal animal = new Dog();
+		
+		animal.eat();
+		animal.move();
 
 	}
 
